0a07e64258795f72544d7f27694725c60e2cbb61
K1fight <92151239+K1fight@users.noreply.github.com>
2023-10-01 08:50:20 -0400

    V0.7(Action complete)

--- snake/src/Logic/Move.java
+++ snake/src/Logic/Move.java
@@ -5,23 +5,28 @@ import UI.BlockPaint;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 
 public class Move implements ActionListener {
-    private List<BlockPaint> snake;
+    private LinkedList<BlockPaint> snake;
     private Timer timer;
     private char key;
+    private int length;
+    private BlockPaint head;
     public Move(){
+        length = 3;
         snake = new LinkedList<>();
         snake.add(new BlockPaint(620,340));
         snake.add(new BlockPaint(640,340));
         snake.add(new BlockPaint(660,340));
+        head = snake.getFirst();
 
         key = 'a';
 
-        timer = new Timer(500,this);
+        timer = new Timer(100,this);
         timer.start();
 
     }
@@ -32,24 +37,32 @@ public class Move implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
+        int x1 ;
+        int x2 = head.getX();
+        int y1 ;
+        int y2 = head.getY();
+
         if(key=='a'){
-            for(BlockPaint s : snake){
-                s.setX(s.getX()-20);
-            }
+            head.setX(head.getX()-20);
         }
         else if(key =='d'){
-            for(BlockPaint s : snake){
-                s.setX(s.getX()+20);
-            }
+            head.setX(head.getX()+20);
         }
         else if(key=='w'){
-            for(BlockPaint s : snake){
-                s.setY(s.getY()-20);
-            }
+            head.setY(head.getY()-20);
         }
         else if (key=='s') {
-            for(BlockPaint s : snake){
-                s.setY(s.getY()+20);
+            head.setY(head.getY()+20);
+        }
+
+        for(BlockPaint s : snake) {
+            x1 = x2;
+            y1 = y2;
+            if (!snake.getFirst().equals(s)) {
+                x2 = s.getX();
+                s.setX(x1);
+                y2 = s.getY();
+                s.setY(y1);
             }
         }
     }
